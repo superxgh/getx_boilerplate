4c6493b78d4574eb88f0b2a90773657375a72196
superxgh <mrsiwakorn@gmail.com>
2023-02-19 09:54:49 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# superx_getx_boilerplate
+
+## master branch: Fluter 3.7.3 version
---
+++ android/app/src/main/kotlin/com/example/superx_getx_boilerplate/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.superx_getx_boilerplate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
